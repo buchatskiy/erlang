704ab8165a8533d2640633ccbfa49ccf707e960a
buchatskiy <buchatskiy2@yandex.ru>
2015-06-18 16:29:11 +0300

    small improvement

--- HW2/bs03.erl
+++ HW2/bs03.erl
@@ -2,19 +2,19 @@
 -export([split/2]).
 split(BinaryData, Spl)->
     split(BinaryData, [], <<>>, list_to_binary(Spl)).
-split(<<X, B/binary>>, L, Acc, <<X, Spl2/binary>>)->
-    check(B, L, Acc, Spl2, <<X, Spl2/binary>>, <<Acc/binary, X>>);
+split(<<X, B/binary>>, L, Acc, <<X, Spl/binary>>)->
+    Size=byte_size(Spl),
+    Spl0 = <<X, Spl/binary>>,
+    case B of
+        <<Spl:Size/binary, T/binary>>->
+            split(T, [Acc|L], <<>>, Spl0);
+        <<S, T/binary>>->
+            split(T, L, <<Acc/binary, X, S>>, Spl0)
+    end;
 split(<<S, B/binary>>, L, Acc, Spl)->
     split(B, L, <<Acc/binary, S>>, Spl);
 split(<<_/binary>>, L, Acc, _)->
     reverse([Acc|L]).
-
-check(B, L, Acc, <<>>, Spl0, _)->
-    split(B, [Acc|L], <<>>, Spl0);
-check(<<X, B/binary>>, L, Acc, <<X, Spl/binary>>, Spl0, Acc2)->
-    check(B, L, Acc, Spl, Spl0, <<Acc2/binary, X>>);
-check(B, L, _, _, Spl0, Acc2)->
-    split(B, L, Acc2, Spl0).
 
 reverse(L)->
     reverse(L,[]).
--- HW2/bs04.erl
+++ HW2/bs04.erl
@@ -5,31 +5,29 @@ decode_xml(<<>>)->
     " ";
 decode_xml(<<"<", B/binary>>)->
     decode_xml(B, <<>>);
-decode_xml(<<" ", B/binary>>)->
-    decode_xml(B);
-decode_xml(<<"\t", B/binary>>)->
-    decode_xml(B);
-decode_xml(<<"\n", B/binary>>)->
-    decode_xml(B);
 decode_xml(<<S, B/binary>>)->
-    [<<S, B/binary>>].
+    case <<S>> of
+        <<" ">> -> decode_xml(B);
+        <<"\t">> -> decode_xml(B);
+        <<"\n">> -> decode_xml(B);
+        _->[<<S, B/binary>>]
+    end.
+
 decode_xml(<<">", B/binary>>, Tag)->
     get_end_tag(B, <<Tag/binary, ">">>, <<>>, Tag);
 decode_xml(<<S, B/binary>>, Tag)->
     decode_xml(B, <<Tag/binary, S>>).
 
-get_end_tag(<<"</", B/binary>>, Tag, Acc, _)->
-    check(B, Tag, Acc, Tag, <<Acc/binary, "</">>);
+get_end_tag(<<"</", B/binary>>, Tag, Acc, Tag_text)->
+    Size = byte_size(Tag),
+    io:format("B: ~p, Tag: ~p\n", [B, <<Tag:Size/binary>>]),
+    case B of
+        <<Tag:Size/binary, T/binary>>->
+            case decode_xml(T)=:=" " of
+                true->{Tag_text, [], decode_xml(Acc)};
+                _->[{Tag_text, [], decode_xml(Acc)}, decode_xml(T)] end;
+        _ ->
+            get_end_tag(B, Tag, <<Acc/binary, "</">>, Tag_text)
+    end;
 get_end_tag(<<S, B/binary>>, Tag, Acc, Tag_text)->
     get_end_tag(B, Tag, <<Acc/binary, S>>, Tag_text).
-
-check(<<>>, <<>>, Acc, Tag_text, _)->
-    {Tag_text, [], decode_xml(Acc)};
-check(B, <<>>, Acc, Tag_text, _)->
-    case decode_xml(B)=:=" " of
-        true->{Tag_text, [], decode_xml(Acc)};
-        _->[{Tag_text, [], decode_xml(Acc)}, decode_xml(B)] end;
-check(<<X, B/binary>>, <<X, Tag/binary>>, Acc, Tag_text, Acc2)->
-    check(B, Tag, Acc, Tag_text, <<Acc2/binary, X>>);
-check(B, _, _, Tag_text, Acc2)->
-    get_end_tag(B, Tag_text, Acc2, Tag_text).
